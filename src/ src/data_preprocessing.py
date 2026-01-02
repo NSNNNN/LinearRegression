ea2a291e56fbfa2bfd635c7b71e5411c2b3be52d
Niyayeshmaghsoodi <145209788+Niyayeshmaghsoodi@users.noreply.github.com>
2026-01-02 21:10:25 +0330

    Create data_preprocessing.py

---
+++ src/ src/data_preprocessing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def preprocess_data(df):
+    X = df.drop("MEDV", axis=1)
+    y = df["MEDV"]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    return train_test_split(X_scaled, y, test_size=0.2, random_state=42)
